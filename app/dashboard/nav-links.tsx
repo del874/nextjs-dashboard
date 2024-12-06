5e156a00e6991df0a9df4c31710827e502e184e1
del874 <changegetter@gmail.com>
2024-12-06 16:39:17 +0800

    next

---
+++ app/dashboard/nav-links.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import Link from 'next/link';
+import { HomeIcon, UserIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'; // Add this import statement
+
+
+
+
+const links = [
+    { href: '/dashboard', name: 'dashboard', icon: HomeIcon },
+    { href: '/dashboard/invoices', name: 'invoices', icon: DocumentDuplicateIcon },
+    { href: '/dashboard/customers', name: 'customers', icon: UserIcon }]
+
+const NavLinks = () => {
+    const pathname = usePathname();
+
+
+  return (
+    <nav className="flex space-x-4">
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+        <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        )
+    })}
+    </nav>
+  );
+};
+
+export default NavLinks;
